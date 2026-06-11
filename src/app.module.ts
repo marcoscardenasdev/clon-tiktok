79e1a935e1f3df1d7bec4fb2a2dab6ee637e9772
marcoscardenasdev <marcos.cardenas.dev@gmail.com>
2026-06-10 21:19:44 -0600

    feat: add password field to User model; update environment variables and dependencies for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { VideosModule } from './videos/videos.module';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [VideosModule, UsersModule, CommentsModule],
+  imports: [VideosModule, UsersModule, CommentsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
